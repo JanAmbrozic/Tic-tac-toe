72435fde0deba453af170c4fb2595722b807fbb8
Jan <janambrozic@Jans-iMac.local>
2016-09-08 23:15:35 +0200

    feat(board): add board basic board drawing animation

--- src/game.js
+++ src/game.js
@@ -1,18 +1,21 @@
 import * as board from 'board.js';
+import TWEEN from 'tween.js';
 
-var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0xd1cfe2});
+console.log(TWEEN)
+let renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0xf6f5ae});
 document.body.appendChild(renderer.view);
 
 // create the root of the scene graph
-var stage = new PIXI.Container();
+let stage = new PIXI.Container();
 
 // start animating
 animate();
-function animate() {
+function animate(time) {
     requestAnimationFrame(animate);
-
+    //update tweens
+    TWEEN.update(time);
     // render the container
     renderer.render(stage);
 }
 
-board.drawBoard()
+board.drawBoard(stage)
